434e468ba1a2011675e6091c426ac4c58ec0bb31
Gabriel Alexander <galexandrove@gmail.com>
2022-12-27 15:31:30 +0000

    Segundo Cambio

--- src/figuras/Test.java
+++ src/figuras/Test.java
@@ -1,19 +1,24 @@
 package figuras;
 
-import utilidades.Circulo;
-
+/**
+ * Actividad 6.1 - Proyecto DAM_ETS
+ * @author Gabbriel Alexander Osorio Osorio
+ *
+ * clase principal de nuestro proyecto para la realización de la actividad
+ */
 public class Test {
     public static void main(String[] args) {
 
-    Circulo c1 = new Circulo(5.5);
-    Circulo c2 = new Circulo(10.1);
-    Circulo c3 = new Circulo(11.9);
+        //Creación de tres objetos del tipo de la clase Circunferencia
+        Circunferencia c1 = new Circunferencia(5.5);
+        Circunferencia c2 = new Circunferencia(10.1);
+        Circunferencia c3 = new Circunferencia(10.9);
 
-    if (c2.esIgual(c3, false))
-        System.out.println("c2 y c3: iguales sin considerar decimales");
+        if (c2.esIgual(false, c3))
+            System.out.println("c2 y c3: iguales sin considerar decimales");
 
-    if (c2.esIgual(c3, true))
-        System.out.println("c2 y c3: iguales considerando decimales");
+        if (c2.esIgual(true, c3))
+            System.out.println("c2 y c3: iguales considerando decimales");
 
     }
 }
